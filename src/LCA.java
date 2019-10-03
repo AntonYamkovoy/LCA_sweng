994cf932f98ddcd5542d69201486b06dd5ab031d
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2019-10-03 10:53:35 +0100

    Added Arbitrary Number of targets for LCA function
    added tests and code for the different cases for LCA-multiple-target

--- src/LCA.java
+++ src/LCA.java
@@ -16,7 +16,7 @@ class TreeNode {
 
 public class LCA {
 
-
+    // takes in 2 nodes in a binary tree rooted at "root", and finds their LCA
 	public  static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 		if( root == null)           return null;
         if( root == p || root == q) return root;
@@ -30,5 +30,24 @@ public class LCA {
 
         
     }
+	
+	// Takes in an arbitrary amount of target nodes for a binary tree, and finds their LCA
+	public static TreeNode lowestCommonAncestorMultiple(TreeNode root, TreeNode[] targets) {
+		if(targets.length < 2) {
+			return null;
+		}
+		if(targets.length == 2) {
+			return lowestCommonAncestor(root,targets[0],targets[1]);
+		}
+		TreeNode result = null;
+		TreeNode temp = lowestCommonAncestor(root,targets[0],targets[1]);
+		for(int i=2; i < targets.length; i++) {
+			 result = lowestCommonAncestor(root,temp,targets[i]);
+			
+		}
+		return result;
+		
+		
+	}
 }
 
--- src/LCATesting.java
+++ src/LCATesting.java
@@ -7,6 +7,118 @@ import org.junit.jupiter.api.Test;
 
 class LCATesting {
 
+	
+	
+	@Test 
+	void lcaNullTest() {
+		TreeNode root = null;
+		assertEquals("Testing LCA, root = null",
+				LCA.lowestCommonAncestor(root, root,root), null);
+		
+		
+	}
+	
+	
+	@Test
+	void lcaRootTest() {
+		TreeNode root = new TreeNode(1);
+		assertEquals("Testing LCA, only root",
+				LCA.lowestCommonAncestor(root, root,root),root);
+		
+	}
+	
+	
+	@Test
+	void lcaSmallTest() {
+		TreeNode root = new TreeNode(1);                            
+		root.left = new TreeNode(3); 
+		root.right = new TreeNode(2);
+	
+	
+		//                   1
+		//                  /\
+		//                 3  2
+
+		TreeNode result = LCA.lowestCommonAncestor(root,root.left, root.right);
+		assertEquals("Testing LCA, node 0 and node 2, Expected result = root",
+				result,root);
+	}
+	
+	
+	@Test
+	void lcaBalancedTreeTest() {
+		TreeNode root = new TreeNode(1);                            
+		root.left = new TreeNode(2); 
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		root.right.left = new TreeNode(6);
+		root.right.right = new TreeNode(7);
+	
+	
+		//                  1
+		//                  /\
+		//                 2  3
+		//                /\  /\
+		//               4  5 6 7
+
+		TreeNode result = LCA.lowestCommonAncestor(root,root.left.left, root.right.left);
+		assertEquals("Testing LCA, node 4 and node 6, Expected result = root",
+				result,root);
+	}	
+		
+	
+	@Test
+	void lcaUnbalancedTreeTest() {
+		TreeNode root = new TreeNode(1);                            
+		root.left = new TreeNode(2); 
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+
+	
+	
+		//                  1
+		//                  /\
+		//                 2  3
+		//                /\  
+		//               4  5  
+
+		TreeNode result = LCA.lowestCommonAncestor(root,root.left.left, root.right);
+		assertEquals("Testing LCA, node 4 and node 2, Expected result = root",
+				result,root);
+		
+	}
+	
+	
+	
+	@Test
+	void lcaStraightLineTree() {
+		TreeNode root = new TreeNode(0);
+		root.right = new TreeNode(5);
+		root.right.right = new TreeNode(4);
+		root.right.right.right = new TreeNode(1);
+		TreeNode result = LCA.lowestCommonAncestor(root, root.right ,root.right.right);
+		assertEquals("Testing LCA,Straight Line Test, node 1 and node 4,Result = "+ result.value+" , Expected result = root",
+				result, root.right);
+		
+		
+		//                   0
+		//                    \
+		//                     5
+		//                      \
+		//                       4
+		//                        \
+		//                         1
+		//                          
+		//                           
+		//                          
+		// 
+		
+	}
+	
+	
+	
 	@Test
 	void lcaNormalTest_1() {
 		TreeNode root = new TreeNode(10);                            
@@ -90,51 +202,10 @@ class LCATesting {
 				result, root.right);
 	}
 	
-	@Test
-	void lcaRootTest() {
-		TreeNode root = new TreeNode(1);
-		assertEquals("Testing LCA, only root",
-				LCA.lowestCommonAncestor(root, root,root),root);
-		
-	}
 	
-	
-	@Test 
-	void lcaNullTest() {
-		TreeNode root = null;
-		assertEquals("Testing LCA, root = null",
-				LCA.lowestCommonAncestor(root, root,root), null);
-		
-		
-	}
 	
 	@Test
-	void lcaStraightLineTree() {
-		TreeNode root = new TreeNode(0);
-		root.right = new TreeNode(5);
-		root.right.right = new TreeNode(4);
-		root.right.right.right = new TreeNode(1);
-		TreeNode result = LCA.lowestCommonAncestor(root, root.right ,root.right.right);
-		assertEquals("Testing LCA,Straight Line Test, node 1 and node 4,Result = "+ result.value+" , Expected result = root",
-				result, root.right);
-		
-		
-		//                   0
-		//                    \
-		//                     5
-		//                      \
-		//                       4
-		//                        \
-		//                         1
-		//                          
-		//                           
-		//                          
-		// 
-		
-	}
-	
-	@Test
-	void lcaBalancedTreeTest() {
+	void lcaMultipleTarget_1() {
 		TreeNode root = new TreeNode(1);                            
 		root.left = new TreeNode(2); 
 		root.right = new TreeNode(3);
@@ -150,34 +221,114 @@ class LCATesting {
 		//                /\  /\
 		//               4  5 6 7
 
-		TreeNode result = LCA.lowestCommonAncestor(root,root.left.left, root.right.left);
-		assertEquals("Testing LCA, node 4 and node 6, Expected result = root",
+		TreeNode temp = LCA.lowestCommonAncestor(root,root.left.left, root.right.left);
+		TreeNode result = LCA.lowestCommonAncestor(root,root.right,temp);
+			
+		assertEquals("Testing LCA, node 4 and node 6 and node 3 Expected result = root",
 				result,root);
 	}	
-		
 	
 	@Test
-	void lcaUnbalancedTreeTest() {
+	void lcaMultipleTarget_2() {
 		TreeNode root = new TreeNode(1);                            
 		root.left = new TreeNode(2); 
-		root.right = new TreeNode(3);
-		root.left.left = new TreeNode(4);
-		root.left.right = new TreeNode(5);
-
+		root.right = new TreeNode(4);
+		root.left.left = new TreeNode(7);
+		root.left.right = new TreeNode(3);
+		root.right.left = new TreeNode(6);
+		root.right.right = new TreeNode(9);
+		root.left.left.left = new TreeNode(0);
+		root.right.right.right = new TreeNode(10);
+		root.right.right.left = new TreeNode(11);
 	
 	
-		//                  1
+		//                   1
 		//                  /\
-		//                 2  3
-		//                /\  
-		//               4  5  
+		//                2    4
+		//               /\    /\
+		//             7  3   6  9
+		//            /           /\
+		//           0           11 10
+		//            
+		//            
+		TreeNode temp = LCA.lowestCommonAncestor(root, root.right.right.right, root.right.right.left);
+		TreeNode result = LCA.lowestCommonAncestor(root, root.right.left, temp);
+		assertEquals("Testing LCA, node 11 node 10 node 6, Expected result = Node 4",
+				result, root.right);
+	}
 
-		TreeNode result = LCA.lowestCommonAncestor(root,root.left.left, root.right);
-		assertEquals("Testing LCA, node 4 and node 2, Expected result = root",
-				result,root);
+	
+	@Test
+	void lcaMultipleTarget_3() {
+		TreeNode root = new TreeNode(1);                            
+		root.left = new TreeNode(2); 
+		root.right = new TreeNode(4);
+		root.left.left = new TreeNode(7);
+		root.left.right = new TreeNode(3);
+		root.right.left = new TreeNode(6);
+		root.right.right = new TreeNode(9);
+		root.left.left.left = new TreeNode(0);
+		root.right.right.right = new TreeNode(10);
+		root.right.right.left = new TreeNode(11);
+	
+	
+		//                   1
+		//                  /\
+		//                2    4
+		//               /\    /\
+		//             7  3   6  9
+		//            /           /\
+		//           0           11 10
+		//            
+		//            
+		TreeNode[] targets = {root.right.right.right, root.right.right.left,root.right.left};
+		TreeNode result = LCA.lowestCommonAncestorMultiple(root, targets);
 		
+		assertEquals("Testing LCA, node 11 node 10 node 6, Expected result = Node 4",
+				result, root.right);
 	}
 	
+	
+	
+	@Test
+	void lcaMultipleTarget_Null() {
+		TreeNode root = null;                           
+           
+		TreeNode[] targets = {root, root};
+		TreeNode result = LCA.lowestCommonAncestorMultiple(root, targets);
+		
+		assertEquals("Testing multiple for null case",
+				result, null);
+	}
+	
+	@Test
+	void lcaMultipleTarget_Root() {
+		TreeNode root = new TreeNode(1);                           
+           
+		TreeNode[] targets = {root, root};
+		TreeNode result = LCA.lowestCommonAncestorMultiple(root, targets);
+		
+		assertEquals("Testing multiple for root case",
+				result, root);
+	}
+	
+	
+	@Test
+	void lcaMultipleTarget_LessThan2Params() {
+		TreeNode root = new TreeNode(1);                           
+           
+		TreeNode[] targets = {root};
+		TreeNode result = LCA.lowestCommonAncestorMultiple(root, targets);
+		
+		assertEquals("Testing multiple for less than 2 params case",
+				result, null);
+	}
+	
+
+	
+
+	
+
 	
 	
 
