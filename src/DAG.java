3168e6929969b76f6f04ae9c5092f1e483b00656
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2019-10-09 10:38:33 +0100

    added skelleton code for dag class

--- src/DAG.java
+++ src/DAG.java
@@ -1,4 +1,54 @@
+import java.util.ArrayList;
 
 public class DAG {
 
+	
+	int vertices;
+	int edges;
+	ArrayList<Integer> adjacencyList;
+	int[] outdegree;
+	int[] indegree;
+	
+	boolean[] visitedVert;
+	boolean[] stack;
+	boolean isCyclic;
+	
+	public DAG(int vertices) {
+		
+		
+	}
+	
+	public void addEdge(int v, int w) {
+		
+		
+	}
+	
+	int checkVertex(int v) {
+		
+		return 0;
+	}
+	
+	int indegree(int v) {
+		
+		return 0;
+	}
+	
+	int outdegree(int v) {
+		
+		return 0;
+	}
+	
+	public void findLoops(int v) {
+		
+		
+	}
+	
+	public DAG reverseDAG() {
+		DAG reverse = new DAG(vertices);
+		
+		return reverse;
+	}
+	
+	
+	
 }
