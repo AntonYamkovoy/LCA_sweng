e699527984bd5f8b2b1c2a85eae06415f306336e
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2019-10-11 13:34:17 +0100

    adding tests

--- src/DAGTesting.java
+++ src/DAGTesting.java
@@ -25,18 +25,18 @@ class DAGTesting {
 	
 	@Test
 	void DAG_init_test_3() {
-		DAG graph = new DAG(10);
+		DAG graph = new DAG(8);
 		
 		graph.addEdge(1, 2);
+		graph.addEdge(1, 3);
+		graph.addEdge(3, 4);
 		graph.addEdge(2, 4);
-		graph.addEdge(2, 5);
-		graph.addEdge(4, 6);
-		graph.addEdge(4, 7);
+	
 		
-		assertEquals(1, graph.indegree(4));
-		assertEquals(2, graph.outdegree(4));
-		assertEquals(5, graph.edges);
-		assertEquals(10, graph.vertices);
+		assertEquals(2, graph.indegree(4));
+		assertEquals(0, graph.outdegree(4));
+		assertEquals(4, graph.edges);
+		assertEquals(8, graph.vertices);
 
 	}
 	@Test
@@ -57,5 +57,28 @@ class DAGTesting {
 		assertEquals(14, graph.vertices);
 
 	}
+	
+	@Test 
+	void test_Indegree() {
+		
+		DAG graph = new DAG(4);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 3);
+		assertEquals(1,graph.indegree(2)); // valid vertex
+		assertEquals(-1,graph.indegree(9)); //invalid vertex
+		
+	}
+	
+	@Test 
+	void test_outdegree() {
+		
+		DAG graph = new DAG(4);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 3);
+		assertEquals(1,graph.outdegree(2)); // valid vertex
+		assertEquals(-1,graph.outdegree(9)); //invalid vertex
+		
+	}
+
 
 }
