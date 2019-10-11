ef0484fed6aa93b9ac942092e2b8f6aeeebe2d2b
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2019-10-11 10:21:05 +0100

    added code for adjacenyList graph structure
    adjacency list implementation of graph structure added

--- src/DAG.java
+++ src/DAG.java
@@ -5,7 +5,7 @@ public class DAG {
 	
 	int vertices;
 	int edges;
-	ArrayList<Integer> adjacencyList;
+	ArrayList<Integer>[] adjacencyList;
 	int[] outdegree;
 	int[] indegree;
 	
@@ -14,41 +14,92 @@ public class DAG {
 	boolean isCyclic;
 	
 	public DAG(int vertices) {
+		if(vertices > 0) {
+			
+			this.vertices = vertices;
+			this.edges = 0;
+			
+			adjacencyList =(ArrayList<Integer>[]) new ArrayList[vertices];
+			
+			indegree = new int[vertices];
+			visitedVert = new boolean[vertices];
+			stack = new boolean[vertices];
+			
+			for(int i=0; i < vertices; i++) 
+				adjacencyList[i] = new ArrayList<Integer>();	
+		}
 		
-		
+	}
+	
+	boolean checkVertex(int v) {
+		if(v < 0 || v >= vertices) return false;
+		return true;	
 	}
 	
 	public void addEdge(int v, int w) {
-		
+		if(checkVertex(v) && checkVertex(w)) {
+			adjacencyList[v].add(w);
+			indegree[w]++;
+			edges++;
+		}
+		else {
+			System.out.println("vertex number out of range: Enter a number between 0 and "+(vertices-1));
+			
+		}
 		
 	}
-	
-	int checkVertex(int v) {
-		
-		return 0;
-	}
+
 	
 	int indegree(int v) {
-		
-		return 0;
+		if(checkVertex(v)) {
+			return indegree[v];
+		}
+		return -1;
 	}
 	
 	int outdegree(int v) {
-		
-		return 0;
+		if(checkVertex(v)) {
+			return adjacencyList[v].size();
+		}
+		return -1;
 	}
 	
-	public void findLoops(int v) {
+	public Iterable<Integer> adj(int v)
+	{
+		return adjacencyList[v];
+	}
+	
+	public void findLoop(int v) {
+		visitedVert[v] = true;
+		stack[v] = true;
 		
+		for(int w : adj(v)) {
+			if(visitedVert[w] = false) {
+				findLoop(w);
+			}
+			else if(stack[w]) {
+				isCyclic = true;
+				return;
+			}
+			
+			
+		}
+		stack[v] = false;
 		
 	}
 	
-	public DAG reverseDAG() {
+	public DAG reverse()
+	{
 		DAG reverse = new DAG(vertices);
-		
+		for(int v = 0; v <vertices; v++)
+		{
+			for(int w : adj(v))
+			{
+				reverse.addEdge(w, v);
+			}		
+		}
 		return reverse;
 	}
-	
 	
 	
 }
